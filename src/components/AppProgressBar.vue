653f7170462bbf9edd830fa64a727e977ea4d0fd
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2024-12-10 16:46:37 -0300

    FEAT: Add AppProgressBar component.

---
+++ src/components/AppProgressBar.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+  defineProps<{
+    progress: number;
+    color: string;
+  }>();
+</script>
+
+<template>
+  <div class="progress-bar">
+    <div class="progress-bar__progress" :style="{ width: progress + '%' }" :class="color"></div>
+  </div>
+  
+</template>
+
+<style lang="postcss" scoped>
+  .progress-bar {
+    @apply bg-gray-200 dark:bg-gray-700 h-2 rounded-md flex justify-start;
+  }
+
+  .progress-bar__progress {
+    @apply h-2 rounded-md;
+  }
+</style>
